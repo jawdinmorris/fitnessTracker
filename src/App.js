6564a7874f875c215be5aa751f420dea9d3848d1
Jordan Morris <jordanandrewmorris1@gmail.com>
2020-06-19 16:21:18 +1000

    main structure and nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+import logo from "./logo192.png";
 import Button from "react-bootstrap/Button";
 import "./App.css";
 
@@ -7,9 +7,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>This is a test</h1>
-        <Button>This is a Button</Button>
+        <img src={logo} alt="Logo" className="logo" />
+        <nav>
+          <ul>
+            <li>
+              <a href="#exercises">Exercises</a>
+            </li>
+            <li>
+              <a href="#add-workout">Add Workout</a>
+            </li>
+            <li>
+              <a href="#progress">Progress</a>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <section id="progress">
+        <h2>Progress</h2>
+      </section>
+      <section id="add-workout">
+        <h2>Add Workout</h2>
+      </section>
+      <section id="exercises">
+        <h2>Exercises</h2>
+      </section>
     </div>
   );
 }
